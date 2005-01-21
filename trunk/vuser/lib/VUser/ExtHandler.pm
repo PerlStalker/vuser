91aa72f3fa44b0ac7a7aff7bfb94f1342cf9a7f3
perlstalker <perlstalker@0b9bf56e-a877-11dd-8184-3158da6531e3>
2005-01-21 21:09:03 +0000

    Use $self not $eh. D'oh!

--- trunk/vuser/lib/VUser/ExtHandler.pm
+++ trunk/vuser/lib/VUser/ExtHandler.pm
@@ -3,9 +3,9 @@ use warnings;
 use strict;
 
 # Copyright 2004 Randy Smith
-# $Id: ExtHandler.pm,v 1.6 2005-01-21 21:07:47 perlstalker Exp $
+# $Id: ExtHandler.pm,v 1.7 2005-01-21 21:09:03 perlstalker Exp $
 
-our $REVISION = (split (' ', '$Revision: 1.6 $'))[1];
+our $REVISION = (split (' ', '$Revision: 1.7 $'))[1];
 our $VERSION = $main::VERSION;
 
 use lib qw(..);
@@ -217,7 +217,7 @@ sub run_tasks
     }
 
     # Check for required options
-    my $opt = $eh->check_required ($keyword, $action, \%opts);
+    my $opt = $self->check_required ($keyword, $action, \%opts);
     if ($opt) {
 	die "Missing required option '$opt'.\n";
     }
