a86bcc96f5cb4ffd427655a3cf9f6c4f6445cbda
perlstalker <perlstalker@0b9bf56e-a877-11dd-8184-3158da6531e3>
2005-03-24 20:53:52 +0000

    Add more tests and put the package line at the top.

--- trunk/vuser/lib/VUser/SOAP.pm
+++ trunk/vuser/lib/VUser/SOAP.pm
@@ -1,4 +1,4 @@
-#! /usr/bin/perl -w
+package VUser::SOAP;
 
 # SOAP interface to VUser.  
 #
@@ -6,14 +6,14 @@
 use warnings;
 use strict;
 
+# $Id: SOAP.pm,v 1.2 2005-03-24 20:53:52 perlstalker Exp $
+
 use vars qw(@ISA);
 
-our $REVISION = (split (' ', '$Revision: 1.1 $'))[1];
+our $REVISION = (split (' ', '$Revision: 1.2 $'))[1];
 our $VERSION = $main::VERSION;
 
 use VUser::CORE;
-
-package VUser::SOAP;
 
 sub hi {                        
   return "hello, world\n";        
@@ -23,6 +23,33 @@ sub hi {
 sub CORE_version {
   return VUser::CORE::version();
   #return "test\n";
+}
+
+sub hash_test {
+    my $class = shift;
+    my %hash = @_;
+    print "Class: $class\n";
+    use Data::Dumper; print Dumper \%hash;
+    return 1;
+}
+
+sub AUTOLOAD
+{
+    use vars '$AUTOLOAD';
+    my $class = shift;
+    my $name = $AUTOLOAD;
+    $name =~ s/.*://;
+    print "name: $name\n";
+    if ($name =~ /^([^_]+)_([^_]+)$/) {
+	my $keyword = $1;
+	my $action = $2;
+	print "Key: $keyword Act: $action\n";
+	# Where am I getting $cfg from?
+	my $cfg;
+	eval { $eh->run_tasks($keyword, $action, $cfg, %opts); };
+    } else {
+	return;
+    }
 }
 
 1;
