7f901fd3ed3c1a5e702a4ff4337726d5a9299103
perlstalker <perlstalker@0b9bf56e-a877-11dd-8184-3158da6531e3>
2005-03-25 17:59:24 +0000

    Preliminary ability to run tasks.

--- trunk/vuser/lib/VUser/SOAP.pm
+++ trunk/vuser/lib/VUser/SOAP.pm
@@ -1,29 +1,21 @@
 package VUser::SOAP;
 
-# SOAP interface to VUser.  
-#
-
 use warnings;
 use strict;
 
 # Copyright 2005 Randy Smith
-# $Id: SOAP.pm,v 1.3 2005-03-25 00:16:15 perlstalker Exp $
+# $Id: SOAP.pm,v 1.4 2005-03-25 17:59:24 perlstalker Exp $
 
 use vars qw(@ISA);
 
-our $REVISION = (split (' ', '$Revision: 1.3 $'))[1];
+our $REVISION = (split (' ', '$Revision: 1.4 $'))[1];
 our $VERSION = $main::VERSION;
 
-use VUser::CORE;
+our %cfg;
+our $eh;
 
-sub hi {                        
-  return "hello, world\n";        
-}
-
-# Rather useless example of exposing a VUser function via SOAP.
-sub CORE_version {
-  return VUser::CORE::version();
-  #return "test\n";
+sub version {
+    return $VERSION;
 }
 
 sub hash_test {
@@ -38,6 +30,8 @@ sub AUTOLOAD
 {
     use vars '$AUTOLOAD';
     my $class = shift;
+    my %opts = @_;
+
     my $name = $AUTOLOAD;
     $name =~ s/.*://;
     print "name: $name\n";
@@ -45,12 +39,43 @@ sub AUTOLOAD
 	my $keyword = $1;
 	my $action = $2;
 	print "Key: $keyword Act: $action\n";
-	# Where am I getting $cfg from?
-	my $cfg;
-	eval { $eh->run_tasks($keyword, $action, $cfg, %opts); };
+	eval { $eh->run_tasks($keyword, $action, \%cfg, %opts); };
     } else {
 	return;
     }
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+VUser::SOAP - SOAP interface to VUser.
+
+=head1 SYNOPSIS
+
+=head1 AUTHORS
+
+Mark Bucciarelli <mark@gaiahost.coop>
+Randy Smith <perlstalker@gmail.com>
+
+=head1 LICENSE
+ 
+ This file is part of vuser.
+ 
+ vuser is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ vuser is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with vuser; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+=cut
--- trunk/vuser/vsoapd.pl
+++ trunk/vuser/vsoapd.pl
@@ -1,19 +1,21 @@
 #!/usr/bin/perl
 
 use warnings;
+use strict;
 
-use strict; 
-use SOAP::Lite ('trace'); 
+#use SOAP::Lite ('trace'); 
+use SOAP::Lite;
 
 # Copyright 2005 Mark Bucciarelli
-# $Id: vsoapd.pl,v 1.2 2005-03-23 21:58:29 perlstalker Exp $
+# $Id: vsoapd.pl,v 1.3 2005-03-25 17:58:51 perlstalker Exp $
 
 use Pod::Usage;
 use Getopt::Long;
 use FindBin;
 use Config::IniFiles;
+use SOAP::Transport::HTTP;
 
-our $REVISION = (split (' ', '$Revision: 1.2 $'))[1];
+our $REVISION = (split (' ', '$Revision: 1.3 $'))[1];
 our $VERSION = '0.1.0';
 
 our $DEBUG = 0;
@@ -31,9 +33,29 @@ use vars qw(@etc_dirs);
 use lib (map { "$_/extensions" } @etc_dirs);
 use lib (map { "$_/lib" } @etc_dirs);
 
-use SOAP::Transport::HTTP;
-
 use VUser::SOAP;
+
+my $config_file;
+for my $etc_dir (@etc_dirs)
+{
+    if (-e "$etc_dir/vuser.conf") {
+	$config_file = "$etc_dir/vuser.conf";
+	last;
+    }
+}
+
+if (not defined $config_file) {
+    die "Unable to find a vuser.conf file in ".join (", ", @etc_dirs).".\n";
+}
+
+my %cfg;
+tie %cfg, 'Config::IniFiles', (-file => $config_file);
+
+my $eh = new VUser::ExtHandler (\%cfg);
+
+# This is really ugly and there should be a better way of doing this.
+%VUser::SOAP::cfg = %cfg;
+$VUser::SOAP::eh = $eh;
 
 # don't die on 'Broken pipe' or Ctrl-C
 #$SIG{PIPE} = $SIG{INT} = 'IGNORE';
