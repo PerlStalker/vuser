edf816441fbd4b46fd0f155f534dc40538e69e08
perlstalker <perlstalker@0b9bf56e-a877-11dd-8184-3158da6531e3>
2007-04-11 22:02:58 +0000

    Strip all of the task code out of VUser::SpamAssassin. The SQL code has been moved to VUser-SpamAssassin-SQL. There was no file based code to move yet.

--- trunk/VUser-SpamAssassin/lib/VUser/SpamAssassin.pm
+++ trunk/VUser-SpamAssassin/lib/VUser/SpamAssassin.pm
@@ -3,12 +3,12 @@ use warnings;
 use strict;
 
 # Copyright 2004 Randy Smith
-# $Id: SpamAssassin.pm,v 1.4 2005-12-06 17:52:33 perlstalker Exp $
+# $Id: SpamAssassin.pm,v 1.5 2007-04-11 22:02:58 perlstalker Exp $
 
 use vars qw(@ISA);
 
-our $REVISION = (split (' ', '$Revision: 1.4 $'))[1];
-our $VERSION = "0.2.1";
+our $REVISION = (split (' ', '$Revision: 1.5 $'))[1];
+our $VERSION = "0.3.0";
 
 use VUser::Meta;
 use VUser::ResultSet;
@@ -30,91 +30,15 @@ my %meta = ('username' => VUser::Meta->new(name => 'username',
 					 description => 'Value for option')
 	    );
 
-my $c_sec = 'Extension_SpamAssassin';
+my $c_sec = 'Extension SpamAssassin';
 
-sub config_sample
-{
-    my $fh;
-    my $cfg = shift;
-    my $opts = shift;
-
-    if (defined $opts->{file}) {
-	open ($fh, ">".$opts->{file})
-	    or die "Can't open '".$opts->{file}."': $!\n";
-    } else {
-	$fh = \*STDOUT;
-    }
-
-    print $fh <<'CONFIG';
-[Extension_SpamAssassin]
-# User scores database username and password.
-# The DSN's here are in the same format as defined in the sql/README*
-# files in the SpamAssassin package.
-# This user needs select, insert and delete permissions.
-user_scores_dsn=
-user_scores_username=
-user_scores_password=
-
-# The name of the table being used for user preferences.
-user_scores_table=userprefs
-
-# AWL database username and password.
-# This user needs select, insert and delete permissions.
-user_awl_dsn=
-user_awl_username=
-user_awl_password=
-
-# Auto whitelist table
-user_awl_table=awl
-
-# Bayes database username and password.
-# This user needs select, insert and delete permissions.
-bayes_dsn=
-bayes_username=
-bayes_password=
-
-CONFIG
-    if (defined $opts->{file}) {
-	close $fh;
-    }
-
-}
+sub c_sec { return $c_sec; }
+sub meta { return %meta; }
 
 sub init
 {
     my $eh = shift;
     my %cfg = @_;
-
-#    return unless VUser::ExtLib::check_bool($cfg{$c_sec}{enable});
-
-    # Connect to the database(s)
-    my $scores_dsn = VUser::ExtLib::strip_ws($cfg{$c_sec}{user_scores_dsn});
-
-    if ($scores_dsn) {
-	require DBI;
-	my $scores_user = VUser::ExtLib::strip_ws($cfg{$c_sec}{user_scores_username});
-	my $scores_pass = VUser::ExtLib::strip_ws($cfg{$c_sec}{user_scores_password});
-	$dbs{scores} = DBI->connect($scores_dsn, $scores_user, $scores_pass);
-	die "Unable connect to database: ".DBI->errstr."\n" unless $dbs{scores};
-    }
-
-    my $awl_dsn = VUser::ExtLib::strip_ws($cfg{$c_sec}{user_awl_dsn});
-    if ($awl_dsn) {
-	require DBI;
-	my $awl_user = VUser::ExtLib::strip_ws($cfg{$c_sec}{user_awl_username});
-	my $awl_pass = VUser::ExtLib::strip_ws($cfg{$c_sec}{user_awl_password});
-	$dbs{awl} = DBI->connect($awl_dsn, $awl_user, $awl_pass);
-	die "Unable connect to database: ".DBI->errstr."\n" unless $dbs{awl};
-    }
-
-    my $bayes_dsn = VUser::ExtLib::strip_ws($cfg{$c_sec}{bayes_dsn});
-    if ($bayes_dsn) {
-	require DBI;
-	my $bayes_user = VUser::ExtLib::strip_ws($cfg{$c_sec}{bayes_username});
-	my $bayes_pass = VUser::ExtLib::strip_ws($cfg{$c_sec}{bayes_password});
-	$dbs{bayes} = DBI->connect($bayes_dsn, $bayes_user, $bayes_pass);
-	die "Unable connect to database: ".DBI->errstr."\n" unless $dbs{bayes};
-    }
 
     # SA
     $eh->register_keyword('sa', 'Manage SpamAssassin settings');
@@ -147,13 +71,11 @@ sub init
     $eh->register_action('sa', 'del');
     $eh->register_option('sa', 'del', $meta{'username'}, 1);
     $eh->register_option('sa', 'del', $meta{'option'}, 1);
-    $eh->register_task('sa', 'del', \&sa_del);
 
     # SA-show: Show user settings
     $eh->register_action('sa', 'show');
     $eh->register_option('sa', 'show', $meta{'username'});
     $eh->register_option('sa', 'show', $meta{'option'});
-    $eh->register_task('sa', 'show', \&sa_show);
 
     # Email
     $eh->register_keyword('email');
@@ -161,173 +83,9 @@ sub init
     # Email-del: When an email is deleted, we need to remove all their
     # settings as well.
     $eh->register_action('email', 'del');
-    $eh->register_task('email', 'del', \&sa_delall);
 }
 
 sub unload {};
-
-sub sa_add
-{
-    my $cfg = shift;
-    my $opts = shift;
-
-    my $user = $opts->{username};
-    my $option = $opts->{option};
-    my $value = $opts->{value};
-
-    if ($dbs{scores}) {
-	my $table = VUser::ExtLib::strip_ws($cfg->{$c_sec}{user_scores_table});
-	my $sql = "Insert into $table set username=?, preference=?, value=?";
-	my $sth = $dbs{scores}->prepare($sql)
-	    or die "Database error: ".$dbs{scores}->errstr."\n";
-	$sth->execute($user, $option, $value)
-	    or die "Database error: ".$sth->errstr."\n";
-    } else {
-	# In the future we'll screw with text files here.
-    }
-}
-
-sub sa_mod
-{
-    my $cfg = shift;
-    my $opts = shift;
-
-    my $user = $opts->{username};
-    my $option = $opts->{option};
-    my $value = $opts->{value};
-    my $delete = $opts->{delete};
-
-    if ($delete) {
-	return sa_del($cfg, $opts, @_);
-    } else {
-	if ($dbs{scores}) {
-	    my $table = VUser::ExtLib::strip_ws($cfg->{$c_sec}{user_scores_table});
-	    my $sql = "update $table set value=? where username=? and preference = ?";
-	    my $sth = $dbs{scores}->prepare($sql)
-		or die "Database error: ".$dbs{scores}->errstr."\n";
-	    $sth->execute($value, $user, $option)
-		or die "Database error: ".$sth->errstr."\n";
-	} else {
-	    # File-based stuff
-	}
-    }
-}
-
-sub sa_del
-{
-    my $cfg = shift;
-    my $opts = shift;
-
-    my $user = $opts->{username};
-    my $option = $opts->{option};
-
-    if ($dbs{scores}) {
-	my $table = VUser::ExtLib::strip_ws($cfg->{$c_sec}{user_scores_table});
-	my $sql = "delete from $table where username=? and preference = ?";
-	my $sth = $dbs{scores}->prepare($sql)
-	    or die "Database error: ".$dbs{scores}->errstr."\n";
-	$sth->execute($user, $option)
-	    or die "Database error: ".$sth->errstr."\n";
-    } else {
-	# File-based stuff here.
-    }
-}
-
-sub sa_delall
-{
-    my $cfg = shift;
-    my $opts = shift;
-
-    my $user = $opts->{username};
-
-    # The email extension uses 'account' instead of 'username'
-    $user = $opts->{account} if not $user;
-
-    # Delete the preferences
-    if ($dbs{scores}) {
-	my $table = VUser::ExtLib::strip_ws($cfg->{$c_sec}{user_scores_table});
-	my $sql = "delete from $table where username=?";
-	my $sth = $dbs{scores}->prepare($sql)
-	    or die "Database error: ".$dbs{scores}->errstr."\n";
-	$sth->execute($user)
-	    or die "Database error: ".$sth->errstr."\n";
-    } else {
-	# file-based
-    }
-
-    # Delete the AWL entries
-    if ($dbs{awl}) {
-	my $table = VUser::ExtLib::strip_ws($cfg->{$c_sec}{user_awl_table});
-	my $sql = "delete from $table where username=?";
-	my $sth = $dbs{scores}->prepare($sql)
-	    or die "Database error: ".$dbs{scores}->errstr."\n";
-	$sth->execute($user)
-	    or die "Database error: ".$sth->errstr."\n";
-    } else {
-    }
-
-    # Delete Baysian DB
-    if ($dbs{bayes}) {
-	# There are quite a few tables we need to delete from.
-	# To start, we need to grab the id for the user.
-	my $sql = 'select id from bayes_vars where username = ?';
-	my $sth = $dbs{scores}->prepare($sql)
-	    or die "Database error: ".$dbs{scores}->errstr."\n";
-	$sth->execute($user)
-	    or die "Database error: ".$sth->errstr."\n";
-	my $res = $sth->fetchrow_hashref;
-	
-	if (defined $res) {
-	    my $id = $res->{id};
-
-	    # Delete everything for this user in each table.
-	    foreach my $table (qw(bayes_vars bayes_tokens bayes_seen
-				 bayes_expire)) {
-		$sql = "delete from $table where id=?";
-		my $sth = $dbs{scores}->prepare($sql)
-		    or die "Database error: ".$dbs{scores}->errstr."\n";
-		$sth->execute($user)
-		    or die "Database error: ".$sth->errstr."\n";
-	    }
-	}
-    } else {
-	# file-based stuff here.
-    }
-}
-
-sub sa_show
-{
-    my $cfg = shift;
-    my $opts = shift;
-
-    my $user = $opts->{username};
-    my $option = $opts->{option};
-
-    my $rs = VUser::ResultSet->new;
-    foreach my $meta_name (qw[username option value]) {
-	$rs->add_meta($meta{$meta_name});
-    }
-    
-    if ($dbs{'scores'}) {
-	$user = '%' unless $user;
-	$option = '%' unless $user;
-
-	my $table = VUser::ExtLib::strip_ws($cfg->{$c_sec}{user_scores_table});
-	my $sql = "select * from $table where username like ? and preference like ? order by username,preference";
-	my $sth = $dbs{scores}->prepare($sql)
-	    or die "Database error: ".$dbs{scores}->errstr."\n";
-	$sth->execute($user, $option)
-	    or die "Database error: ".$sth->errstr."\n";
-
-	my $res;
-	while (defined ($res = $sth->fetchrow_hashref)) {
-	    print join (':', $res->{username}, $res->{option}, $res->{value});
-	    print "\n";
-	    $rs->add_data([$res->{username}, $res->{option}, $res->{value}]);
-	}
-    }
-    return $rs;
-}
 
 1;
 
@@ -339,42 +97,17 @@ VUser::SpamAssassin - vuser SpamAssassin support extension
 
 =head1 DESCRIPTION
 
-It is assumed that this module will be used primarily when using a database
-to store SpamAssassin settings. File based configuration is not supported
-at this time but will probably be added at some point in the future.
+VUser::SpamAssassin is a generic extension that provides the keywords and actions used by
+other VUser::SpamAssassin::* extensions. It is not meant to be used by itself.
 
 =head1 CONFIGURATION
 
- [Extension_SpamAssassin]
- # User scores database username and password.
- # The DSN's here are in the same format as defined in the sql/README*
- # files in the SpamAssassin package.
- # This user needs select, insert and delete permissions.
- user_scores_dsn=
- user_scores_username=
- user_scores_password=
- 
- # The name of the table being used for user preferences.
- user_scores_table=userprefs
- 
- # AWL database username and password.
- # This user needs select, insert and delete permissions.
- user_awl_dsn=
- user_awl_username=
- user_awl_password=
- 
- # Auto whitelist table
- user_awl_table=awl
- 
- # Bayes database username and password.
- # This user needs select, insert and delete permissions.
- bayes_dsn=
- bayes_username=
- bayes_password=
+VUser::SpamAssassin doesn't have any configuration options itself. Other VUser::SpamAssassin::*
+extensions may provide additional configuration options.
 
 =head1 AUTHOR
 
-Randy Smith <perlstalker@gmail.com>
+Randy Smith <perlstalker@vuser.org>
 
 =head1 LICENSE
  
