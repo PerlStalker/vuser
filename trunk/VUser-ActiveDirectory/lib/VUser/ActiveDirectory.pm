0cbffe489518269d8110b5ad989577e5067060d6
perlstalker <perlstalker@0b9bf56e-a877-11dd-8184-3158da6531e3>
2008-03-21 15:52:35 +0000

    Fill out AD actions and options.

--- trunk/VUser-ActiveDirectory/lib/VUser/ActiveDirectory.pm
+++ trunk/VUser-ActiveDirectory/lib/VUser/ActiveDirectory.pm
@@ -3,7 +3,7 @@ use warnings;
 use strict;
 
 # Copyright 2008 Randy Smith
-# $Id: ActiveDirectory.pm,v 1.2 2008-03-17 22:54:28 perlstalker Exp $
+# $Id: ActiveDirectory.pm,v 1.3 2008-03-21 15:52:35 perlstalker Exp $
 
 use VUser::Log qw(:levels);
 use VUser::ExtLib qw(:config);
@@ -21,34 +21,45 @@ our %EXPORT_TAGS = (utils => [qw(domain2ldap)]);
 our $log;
 our $c_sec = 'Extension ActiveDirectory';
 our %meta = (
-	'user' => VUser::Meta->('name' => 'user',
+	'user' => VUser::Meta->new('name' => 'user',
 							'type' => 'string',
 							'description' => 'User name'
 							),
-	'password' => VUser::Meta->('name' => 'password',
+	'password' => VUser::Meta->new('name' => 'password',
 	                            'type' => 'string',
 	                            'description' => 'Password'
 	                            ),
-	'domain' => VUser::Meta->('name' => 'domain',
+	'domain' => VUser::Meta->new('name' => 'domain',
 	                          'type' => 'string',
 	                          'description' => 'AD Domain; e.g. ad.yourdomain.com'
 	                          ),
-	'ou' => VUser::Meta->('name' => 'ou',
+	'ou' => VUser::Meta->new('name' => 'ou',
 			              'type' => 'string',
 			              'description' => 'OU to modify; e.g. ou=Regular,ou=Users'
 			              ),
-    'homedir' => VUser::Meta->('name' => 'homedir',
+    'homedir' => VUser::Meta->new('name' => 'homedir',
                                'type' => 'string',
                                'description' => 'Path of user\'s home dir; e.g. \\server\Home\user'
                                ),
-    'homedrive' => VUser::Meta->('name' => 'homedrive',
+    'homedrive' => VUser::Meta->new('name' => 'homedrive',
                                  'type' => 'string',
                                  'description' => 'Drive letter to map home directory to; e.g. N:'
                                  ),
-    'group' => VUser::Meta->('name' => 'group',
+    'group' => VUser::Meta->new('name' => 'group',
                              'type' => 'string',
                              'description' => 'Group name'
-                             )
+                             ),
+    'createhome' => VUser::Meta->new('name' => 'createhome',
+                                        'type' => 'boolean',
+                                        'description' => 'Create the user\'s home directory'
+                                        ),
+    'deletehome' => VUser::Meta->new('name' => 'deletehome',
+                                        'type' => 'boolean',
+                                        'description' => 'Delete the user\'s home directory'
+                                        ),
+    'recurse' => VUser::Meta->new('name' => 'recurse',
+                                  'type' => 'boolean',
+                                  'description' => 'Show members from sub-OUs')
 );
 
 sub c_sec { return $c_sec; }
@@ -80,22 +91,37 @@ sub init {
 	$eh->register_option('aduser', 'add', $meta{'ou'});
 	$eh->register_option('aduser', 'add', $meta{'homedir'});
 	$eh->register_option('aduser', 'add', $meta{'homedrive'});
-	# Option to create homedir
+	$eh->register_option('aduser', 'add', $meta{'createhome'});
 	
 	# aduser del
 	$eh->register_action('aduser', 'del', 'Delete an AD user');
 	$eh->register_option('aduser', 'del', $meta{'user'}, 1);
 	$eh->register_option('aduser', 'del', $meta{'domain'});
 	$eh->register_option('aduser', 'del', $meta{'ou'});
-	# Option to delete homedir
+	$eh->register_option('aduser', 'del', $meta{'deletehome'});
 	
 	# aduser mod
 	$eh->register_action('aduser', 'mod', 'Modify an AD user');
-	$eh->register_action('aduser', 'mod', 'Delete an AD user');
 	$eh->register_option('aduser', 'mod', $meta{'user'}, 1);
 	$eh->register_option('aduser', 'mod', $meta{'domain'});
 	$eh->register_option('aduser', 'mod', $meta{'ou'});
+	$eh->register_option('aduser', 'mod', $meta{'user'}->new('name' => 'useruser'));
+	# For now, the user cannot change domains
+	# $eh->register_option('aduser', 'mod', $meta{'domain'}->new('name' => 'newdomain'));
+	$eh->register_option('aduser', 'mod', $meta{'ou'}->new('name' => 'newou'));
     # More options
+    
+    # aduser enable
+	$eh->register_action('aduser', 'enable', 'Enable an AD user');
+	$eh->register_option('aduser', 'enable', $meta{'user'}, 1);
+	$eh->register_option('aduser', 'enable', $meta{'domain'});
+	$eh->register_option('aduser', 'enable', $meta{'ou'});
+
+    # aduser disable
+	$eh->register_action('aduser', 'disable', 'Disable an AD user');
+	$eh->register_option('aduser', 'disable', $meta{'user'}, 1);
+	$eh->register_option('aduser', 'disable', $meta{'domain'});
+	$eh->register_option('aduser', 'disable', $meta{'ou'});
     
     # aduser change-password
     $eh->register_action('aduser', 'change-password', 'Change an AD user\'s password');
@@ -103,6 +129,76 @@ sub init {
 	$eh->register_option('aduser', 'change-password', $meta{'domain'});
 	$eh->register_option('aduser', 'change-password', $meta{'ou'});
 	$eh->register_option('aduser', 'change-password', $meta{'password'}, 1);
+
+    # aduser list
+    $eh->register_action('aduser', 'list', 'List AD users');
+    $eh->register_option('aduser', 'list', $meta{'domain'});
+    $eh->register_option('aduser', 'list', $meta{'ou'});
+    $eh->register_option('aduser', 'list', VUser::Meta->new('name' => 'showdisabled',
+                                                            'type' => 'boolean',
+                                                            'description' => 'Show disabled accounts'));
+    $eh->register_option('aduser', 'list', VUser::Meta->new('name' => 'onlydisabled',
+                                                            'type' => 'boolean',
+                                                            'description' => 'Only show disabled accounts'));
+    $eh->register_option('aduser', 'list', VUser::Meta->new('name' => 'dayssincelogon',
+                                                            'type' => 'boolean',
+                                                            'description' => 'Number of says since last logon'));
+    $eh->register_option('aduser', 'list', $meta{'recurse'});
+    
+    ## adgroup
+    $eh->register_keyword('adgroup', 'Manage AD groups');
+    
+    # adgroup add
+    $eh->register_action('adgroup', 'add', 'Add a group to AD');
+    $eh->register_option('adgroup', 'add', $meta{'group'}, 1);
+    $eh->register_option('adgroup', 'add', $meta{'domain'});
+    $eh->register_option('adgroup', 'add', $meta{'ou'});
+
+    # adgroup del
+    $eh->register_action('adgroup', 'del', 'Delete a group to AD');
+    $eh->register_option('adgroup', 'del', $meta{'group'}, 1);
+    $eh->register_option('adgroup', 'del', $meta{'domain'});
+    $eh->register_option('adgroup', 'del', $meta{'ou'});
+    
+    # adgroup mod
+    $eh->register_action('adgroup', 'mod', 'Modify a group to AD');
+    $eh->register_option('adgroup', 'mod', $meta{'group'}, 1);
+    $eh->register_option('adgroup', 'mod', $meta{'domain'});
+    $eh->register_option('adgroup', 'mod', $meta{'ou'});
+    $eh->register_option('adgroup', 'mod', $meta{'group'}->new('name' => 'newgroup'));
+    #$eh->register_option('adgroup', 'mod', $meta{'domain'}->new('name' => 'newdomain'));
+    $eh->register_option('adgroup', 'mod', $meta{'ou'}->new('name' => 'newou'));
+
+    # adgroup adduser
+    $eh->register_action('adgroup', 'adduser', 'Add a user to the group');
+    $eh->register_option('adgroup', 'adduser', $meta{'group'}, 1);
+    $eh->register_option('adgroup', 'adduser', $meta{'user'}, 1);
+    $eh->register_option('adgroup', 'adduser', $meta{'domain'});
+    $eh->register_option('adgroup', 'adduser', $meta{'ou'});
+    $eh->register_option('adgroup', 'adduser', $meta{'domain'}->new('name' => 'userdomain'));
+    $eh->register_option('adgroup', 'adduser', $meta{'ou'}->new('name' => 'userou'));
+
+    # adgroup rmuser
+    $eh->register_action('adgroup', 'rmuser', 'Remove a user from the group');
+    $eh->register_option('adgroup', 'rmuser', $meta{'group'}, 1);
+    $eh->register_option('adgroup', 'rmuser', $meta{'user'}, 1);
+    $eh->register_option('adgroup', 'rmuser', $meta{'domain'});
+    $eh->register_option('adgroup', 'rmuser', $meta{'ou'});
+    $eh->register_option('adgroup', 'rmuser', $meta{'domain'}->new('name' => 'userdomain'));
+    $eh->register_option('adgroup', 'rmuser', $meta{'ou'}->new('name' => 'userou'));
+    
+    # adgroup list
+    $eh->register_action('adgroup', 'list', 'List groups');
+    $eh->register_option('adgroup', 'list', $meta{'domain'});
+    $eh->register_option('adgroup', 'list', $meta{'ou'});
+    $eh->register_option('adgroup', 'list', $meta{'recurse'});
+    
+    # adgroup members
+    $eh->register_action('adgroup', 'members', 'List group members');
+    $eh->register_option('adgroup', 'members', $meta{'group'}, 1);
+    $eh->register_option('adgroup', 'members', $meta{'domain'});
+    $eh->register_option('adgroup', 'members', $meta{'ou'});
+    
     
 }
 
